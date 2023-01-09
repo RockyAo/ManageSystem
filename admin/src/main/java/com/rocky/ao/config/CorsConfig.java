3ca069c90dea74e0775a4251dca9c70ad70f36e3
Yun Ao <yun.ao@ninebot.com>
2023-01-09 15:42:58 +0800

    跨域

---
+++ admin/src/main/java/com/rocky/ao/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.rocky.ao.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author yun.ao
+ * @date 2023/1/9 15:40
+ * @description
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowCredentials(true);
+    }
+}
