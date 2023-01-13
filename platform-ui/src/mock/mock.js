44a9fd691c7c9e103fc0c7c707e14de7580f578d
Yun Ao <yun.ao@ninebot.com>
2023-01-13 17:10:03 +0800

    mockjs tests

---
+++ platform-ui/src/mock/mock.js
@@ -0,0 +1,14 @@
+import Mock from 'mockjs'
+
+Mock.mock('http://localhost:8080/user', {
+  'name': '@name', // 随机生成姓
+  // eslint-disable-next-line
+  'name': '@email', // 随机生成邮箱
+  'age|1-10': 5 // 年龄1-10之间
+})
+
+Mock.mock('http://localhost:8080/menu', {
+  'id': '@increment', // id自增
+  'name': 'menu', // 名称为menu
+  'order|1-20': 5 // 排序1-20之间
+})
--- platform-ui/src/views/Home.vue
+++ platform-ui/src/views/Home.vue
@@ -1,12 +1,39 @@
 <template>
   <div class="page">
     <h2>Home page</h2>
+    <el-button type="primary" @click="testAxios()">测试 Axios </el-button>
+    <el-button type="primary" @click="getUser()">Get User </el-button>
+    <el-button type="primary" @click="getMenu()">Get Menu </el-button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+// eslint-disable-next-line
+import mock from '@/mock/mock.js'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  methods: {
+    testAxios () {
+      axios.get('http://localhost:8080')
+        .then(res => {
+          alert(res.data)
+        })
+    },
+
+    getUser () {
+      axios.get('http://localhost:8080/user').then(res => {
+        alert(JSON.stringify(res.data))
+      })
+    },
+
+    getMenu () {
+      axios.get('http://localhost:8080/menu').then(res => {
+        alert(JSON.stringify(res.data))
+      })
+    }
+  }
 }
 
 </script>
